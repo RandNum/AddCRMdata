096a29c914b46ed9d2275bfb3334215ca36b9295
randnum <wiggins2000@gmail.com>
2018-06-16 13:49:01 +0200

    Added rate limit, backoff and iteration through the entire file. combined smaller functions into a more readable single function

--- AddData.py
+++ AddData.py
@@ -1,75 +1,47 @@
 import requests
 import csv
 import os
-import json
-
-from ratelimit import limits
-
-
+from requests.auth import HTTPBasicAuth
+from ratelimit import limits, RateLimitException
+from backoff import on_exception, expo
 
 user = "wiggins2000@gmail.com"
 pw = "Deadalus82$$"
-from requests.auth import HTTPBasicAuth
 
-def authTest():
-    print("Authenticating...")
-    response = requests.get('https://api.relay42.com/v1/site-1151/', auth=HTTPBasicAuth('wiggins2000@gmail.com', 'Deadalus82$$'))
-    print("response code: " + str(response.status_code))
-#authTest()
+FIVE_MINUTES = 300
 
-
-
-
-
-def parseCSV():
+@on_exception(expo, RateLimitException, max_tries=8)
+@limits(calls=50, period=FIVE_MINUTES)
+def AddFactToProfile():
     print("parsing CSV file")
     filepath = os.path.join('/Users/laptop/Desktop/Relay42/FactDataForTCAssignment/', 'generatedDataFile_small_5.csv')
-    with open(filepath, newline='') as csvfile:
+    filepathBig = os.path.join('/Users/laptop/Desktop/Relay42/FactDataForTCAssignment/', 'generatedDataFile_225000.csv')
+
+    with open(filepathBig, newline='') as csvfile:
         csvData = csv.reader(csvfile, delimiter=';')
+        headers = next(csvData)
         for row in csvData:
             pid = row[0]
             dest = row[1]
             org = row[2]
+            JSONData = [{
+                "factName": "mostSearched",
+                "factTtl": "65000000",
+                "properties":{"PartnerID":pid,
+                              "Dest":dest,
+                              "Orig":org}
+                    }]
 
-    thisJSONObj = convertToAPIFormat(pid, dest, org)
-    return thisJSONObj
+            url2 = "https://api.relay42.com/v1/site-1151/profiles/2001/facts?partnerId=" + pid + "&forceInsert=false"
 
-#parseCSV()
+            headers = {"content-type":"application/json"}
+            # call get service with headers and params
+            print("Adding Fact to PID " + pid)
+            response = requests.post(url2, json=JSONData, headers=headers, auth=HTTPBasicAuth(user, pw))
+            if response.status_code != 200:
+                raise Exception('API response: {}'.format(response.status_code))
+            print("code: " + str(response.status_code))
+            #print("headers: " + str(response.headers))
+            #print("content: " + str(response.text))
 
-
-def convertToAPIFormat(a, b, c):
-    csvList = [{
-        "factName": "mostSearched",
-        "factTtl": "65000000",
-        "properties":{"PartnerID":a,
-                      "Dest":b,
-                      "Orig":c}
-            }]
-
-
-    return csvList
-
-
-def relayPOSTRequest():
-    jsonData = [{
-        "factName": "mostSearched",
-        "factTtl": "65000000",
-        "properties": {"this":"that",
-                       "this":"that"}
-        }]
-
-
-    newJSONData = parseCSV()
-    print(newJSONData)
-
-    url = 'https://api.relay42.com/v1/site-1151/profiles/2001/facts?partnerId=1151&forceInsert=false'
-
-    headers = {"content-type":"application/json"}
-    # call get service with headers and params
-    response = requests.post(url, json=newJSONData, headers=headers, auth=HTTPBasicAuth(user, pw))
-
-    print("code: " + str(response.status_code))
-    print("headers: " + str(response.headers))
-    print("content: " + str(response.text))
-
-relayPOSTRequest()
+AddFactToProfile()
